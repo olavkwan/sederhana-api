21bbfb621d15b3dc5c45431d5b6bc8df8099141a
Olav Sibuea <olavfelix@gmail.com>
2022-11-08 21:11:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const aplikasi = express();
+
+let object =  [
+    {
+        name : "olavkwan",
+        umur : 31,
+    },
+    {
+        name : "kwanolav",
+        umur : 28,
+    },
+];
+    // [] => Data Object
+    // [] => Data Array
+aplikasi.get("/", function(request, response){
+    response.json({ pesan : "Data berhasil di ambil", rizka : object });
+})
+
+aplikasi.listen(3002);
