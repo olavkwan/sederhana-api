900570ccd7228d051e8cf697eeb5019f01fa4958
Olav Sibuea <olavfelix@gmail.com>
2022-11-12 00:48:36 +0700

    penambahan kategori controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 let UserControllers = require("./user.controller");
-
+let KategoriControllers = require('./kategori.controller');
 module.exports = (aplikasi) => {
     aplikasi.use("/user", UserControllers);
+    aplikasi.use("/kategori", KategoriControllers);
 };
